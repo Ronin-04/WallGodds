1c5ba200d113d8cbab45b0d8bc1950e14ab2990c
palchhin <palchhinparihar@gmail.com>
2026-02-03 15:11:27 +0530

    feat: update footer icons and remove unused favicon.

--- Frontend/src/Components/CommonModule/FooterModule/Footer.jsx
+++ Frontend/src/Components/CommonModule/FooterModule/Footer.jsx
@@ -1,22 +1,19 @@
 import Styles from "./Footer.module.css";
-import { SiGmail } from "react-icons/si";
-import { FaGithub, FaLinkedin, FaDiscord } from "react-icons/fa";
 
 export const Footer = () => {
+
     const socialLinks = [
-        {name: "Email", url: "mailto:wallgodds@gmail.com", icon: <SiGmail />},
-        {name: "GitHub", url: "https://github.com/WallGodds", icon: <FaGithub />},
-        {name: "LinkedIn", url: "https://www.linkedin.com/company/wallgodds", icon: <FaLinkedin />},
-        {name: "Discord", url: "https://discord.gg/kTQ5KWANp8", icon: <FaDiscord />},
+        {name: "Email", url: "mailto:wallgodds@gmail.com", path: '/socialLinks/Mail.svg'},
+        {name: "GitHub", url: "https://github.com/WallGodds", path: '/socialLinks/GitHub.svg'},
+        {name: "LinkedIn", url: "https://www.linkedin.com/company/wallgodds", path: '/socialLinks/LinkedIn.svg'},
+        {name: "Discord", url: "https://discord.gg/kTQ5KWANp8", path: '/socialLinks/Discord.svg'},
     ];
 
     return (
         <footer className={Styles.footer}>
             {/* Left */}
             <div className={Styles.left}>
-                <div className={Styles.logoBox}>
-                    <img src="/WallGodds_Favicon_Dark.png" alt="WallGodds Favicon" className={Styles.logoIcon} />
-                </div>
+                <div className={Styles.logoBox} />
                 <span className={Styles.footerText}>A Community Initiative</span>
             </div>
 
@@ -24,7 +21,7 @@ export const Footer = () => {
             <div className={Styles.socials}>
                 {socialLinks.map((link, index) => (
                     <a href={link.url} title={`WallGodds's ${link.name}`} target="_blank" aria-label={link.name} className={Styles.iconBtn} key={index}>
-                        {link.icon}
+                        <img src={link.path} alt={link.name} />
                     </a>
                 ))}
             </div>
